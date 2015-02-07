195d9165d473ed89b48baea05d7dfa11889b8b5d
Ionică Bizău <bizauionica@yahoo.com>
2015-02-07 22:52:00 +0200

    Convert to markdown.

--- src/medium-editor-md.js
+++ src/medium-editor-md.js
@@ -7,13 +7,44 @@
         throw new Error("Medium Editor is not loaded on the page.");
     }
 
-    var he = {
-            decode: {{{he.decode}}}
-        }
+    var he = { decode: {{{he.decode}}} }
       , toMarkdown = {{{toMarkdown}}}
       ;
 
 
     root.toMarkdown = toMarkdown;
-    alert(toMarkdown("<h1>test</h1>"));
+
+    function MeMarkdown(options, callback) {
+        var self = this;
+        if (typeof options === "function") {
+            callback = options;
+            options = {};
+        }
+        options = Object(options);
+        options.events = options.events || ["input", "change"];
+        callback = callback || options.callback || function () {};
+
+        setTimeout(function() {
+            self.element = options.element || "[data-medium-element=true]";
+            if (typeof self.element === "string") {
+                self.element = document.querySelector(self.element);
+            }
+            self.element = self.element || options.selector;
+
+            function handler() {
+                callback(toMarkdown(self.element.innerHTML).split("\n").map(function (c) {
+                    return c.trim();
+                }).join("\n").trim());
+            }
+
+            options.events.forEach(function (c) {
+                self.element.addEventListener(c, handler);
+            });
+
+            handler();
+        }, 0);
+    }
+
+    root.MeMarkdown = MeMarkdown;
+
 })(this);
