90a88b8cddfdd7adb2458703e12df0cf0cb19493
Ionică Bizău <bizauionica@yahoo.com>
2015-02-08 10:25:27 +0200

    JSDoc comments

--- src/medium-editor-md.js
+++ src/medium-editor-md.js
@@ -1,14 +1,34 @@
+/**
+ * MeMarkdown
+ * Creates a new instance of `MeMarkdown`.
+ *
+ * @name MeMarkdown
+ * @function
+ * @param {Object} options An object containing the following fields:
+ *
+ *  - `events` (Array): An array with the events when the markdown code will be generated (default: `["input", "change"]`).
+ *  - `callback` (Function): The callback function. If the second argument is a function, then it has greater priority.
+ *  - `selector` (String|HTMLElement): The Medium Editor element or selector (default: `[data-medium-element=true]`).
+ *
+ * @param {Function} callback The callback function that is called with the markdown code (first argument).
+ * @return {undefined}
+ */
 module.exports = function (options, callback) {
     var self = this;
+
     if (typeof options === "function") {
         callback = options;
         options = {};
     }
+
+    // Defaults
     options = Object(options);
     options.events = options.events || ["input", "change"];
     callback = callback || options.callback || function () {};
 
+    // Wait until the Medium Editor is inited
     setTimeout(function() {
+
         self.element = options.element || "[data-medium-element=true]";
         if (typeof self.element === "string") {
             self.element = document.querySelector(self.element);
