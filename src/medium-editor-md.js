be03ba811ae17149cd4b73b41e364eef97a9b4e0
Ionică Bizău <bizauionica@yahoo.com>
2015-02-08 19:17:37 +0200

    Code style, changed the logic of if statement

--- src/medium-editor-md.js
+++ src/medium-editor-md.js
@@ -8,12 +8,12 @@
  *
  *  - `events` (Array): An array with the events when the markdown code will be generated (default: `["input", "change"]`).
  *  - `callback` (Function): The callback function. If the second argument is a function, then it has greater priority.
- *  - `selector` (String|HTMLElement): The Medium Editor element or selector (default: `[data-medium-element=true]`).
  *
  * @param {Function} callback The callback function that is called with the markdown code (first argument).
  * @return {undefined}
  */
 module.exports = function (options, callback) {
+
     if (typeof options === "function") {
         callback = options;
         options = {};
@@ -25,25 +25,26 @@ module.exports = function (options, callback) {
     callback = callback || options.callback || function () {};
 
     // Called by medium-editor during init
-    this.init = function(meInstance) {
+    this.init = function (meInstance) {
+
         this.me = meInstance;
 
         // If this instance of medium-editor doesn't have any elements, there's nothing for us to do
-        if (this.me.elements.length > 0) {
-            // Element(s) that this instance of medium-editor is attached to is/are stored in .elements
-            this.element = this.me.elements[0];
+        if (!this.me.elements || !this.me.elements.length) { return; }
 
-            var handler = function() {
-                callback(toMarkdown(this.element.innerHTML).split("\n").map(function (c) {
-                    return c.trim();
-                }).join("\n").trim());
-            }.bind(this);
+        // Element(s) that this instance of medium-editor is attached to is/are stored in .elements
+        this.element = this.me.elements[0];
 
-            options.events.forEach(function (c) {
-                this.element.addEventListener(c, handler);
-            }.bind(this));
+        var handler = function () {
+            callback(toMarkdown(this.element.innerHTML).split("\n").map(function (c) {
+                return c.trim();
+            }).join("\n").trim());
+        }.bind(this);
 
-            handler();
-        }
+        options.events.forEach(function (c) {
+            this.element.addEventListener(c, handler);
+        }.bind(this));
+
+        handler();
     };
 };
