cacc56e15344e7bddd78f83218eb9dd366323dfe
Ionică Bizău <bizauionica@yahoo.com>
2015-02-08 09:33:25 +0200

    New dist files (including minified ones).

---
+++ dist/me-markdown.no-deps.js
@@ -0,0 +1,37 @@
+(function (root) {
+    if (typeof MediumEditor !== "function") {
+        throw new Error("Medium Editor is not loaded on the page.");
+    }
+
+    var MeMarkdown = function (options, callback) {
+    var self = this;
+    if (typeof options === "function") {
+        callback = options;
+        options = {};
+    }
+    options = Object(options);
+    options.events = options.events || ["input", "change"];
+    callback = callback || options.callback || function () {};
+
+    setTimeout(function() {
+        self.element = options.element || "[data-medium-element=true]";
+        if (typeof self.element === "string") {
+            self.element = document.querySelector(self.element);
+        }
+        self.element = self.element || options.selector;
+
+        function handler() {
+            callback(toMarkdown(self.element.innerHTML).split("\n").map(function (c) {
+                return c.trim();
+            }).join("\n").trim());
+        }
+
+        options.events.forEach(function (c) {
+            self.element.addEventListener(c, handler);
+        });
+
+        handler();
+    }, 0);
+};
+    root.MeMarkdown = MeMarkdown;
+})(this);
--- dist/me-markdown.standalone.js
+++ dist/me-markdown.standalone.js
@@ -1,7 +1,3 @@
-/**
- * This is the template file for building the embeded Medium Editor Markdown extension.
- * */
-
 (function (root) {
     if (typeof MediumEditor !== "function") {
         throw new Error("Medium Editor is not loaded on the page.");
@@ -251,42 +247,37 @@
 
   return cleanUp(string);
 }
+      , MeMarkdown = function (options, callback) {
+    var self = this;
+    if (typeof options === "function") {
+        callback = options;
+        options = {};
+    }
+    options = Object(options);
+    options.events = options.events || ["input", "change"];
+    callback = callback || options.callback || function () {};
+
+    setTimeout(function() {
+        self.element = options.element || "[data-medium-element=true]";
+        if (typeof self.element === "string") {
+            self.element = document.querySelector(self.element);
+        }
+        self.element = self.element || options.selector;
+
+        function handler() {
+            callback(toMarkdown(self.element.innerHTML).split("\n").map(function (c) {
+                return c.trim();
+            }).join("\n").trim());
+        }
+
+        options.events.forEach(function (c) {
+            self.element.addEventListener(c, handler);
+        });
+
+        handler();
+    }, 0);
+}
       ;
 
-
-    root.toMarkdown = toMarkdown;
-
-    function MeMarkdown(options, callback) {
-        var self = this;
-        if (typeof options === "function") {
-            callback = options;
-            options = {};
-        }
-        options = Object(options);
-        options.events = options.events || ["input", "change"];
-        callback = callback || options.callback || function () {};
-
-        setTimeout(function() {
-            self.element = options.element || "[data-medium-element=true]";
-            if (typeof self.element === "string") {
-                self.element = document.querySelector(self.element);
-            }
-            self.element = self.element || options.selector;
-
-            function handler() {
-                callback(toMarkdown(self.element.innerHTML).split("\n").map(function (c) {
-                    return c.trim();
-                }).join("\n").trim());
-            }
-
-            options.events.forEach(function (c) {
-                self.element.addEventListener(c, handler);
-            });
-
-            handler();
-        }, 0);
-    }
-
     root.MeMarkdown = MeMarkdown;
-
 })(this);
