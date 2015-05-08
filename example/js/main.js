83930020fb8e116d68cb9c4bd4eefcf47bec6f68
Ionică Bizău <bizauionica@yahoo.com>
2015-05-08 10:44:15 +0300

    Added the buttons

--- example/js/main.js
+++ example/js/main.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function () {
     var markDownEl = document.querySelector(".editor > .right > pre");
     new MediumEditor(document.querySelector(".editor > .left"), {
-        extensions: {
+        buttons: ["bold", "italic", "underline", "orderedlist", "unorderedlist", "anchor", "header1", "header2", "quote"]
+      , extensions: {
             markdown: new MeMarkdown(function (md) {
                 markDownEl.innerText = md;
             })
